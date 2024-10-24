4b0fcc95812ae6fe5232015e9372135180320607
1zxcvbnm8 <157445997+shou-go@users.noreply.github.com>
2024-10-24 15:59:38 +0900

    update

--- 00_vivid2d 2.8 pazle/game/src/game/game_object/GameMain/dice/dice.cpp
+++ 00_vivid2d 2.8 pazle/game/src/game/game_object/GameMain/dice/dice.cpp
@@ -1,46 +1,88 @@
 #include "dice.h"
 
+/*お役立ち情報
+* 1 left 0   right 63
+* 2      64        178
+* 3      128       191
+* 4      192       255
+* 5      256       319
+* 6      320       383
+* 
+* マップ大きさ
+* 一辺64×12＝768
+*/
+
+struct DICE
+{
+    int top,
+        right,
+        bottom,
+        left,
+        center,
+        back;
+};
+
+DICE m_Dice = { 2, 3, 5, 4, 1, 6 };
 
 void Dice::Initialize(void)
 {
     m_DiceDigit = 1;//仮置き
+    m_DiceDirection = 1;
     m_Position = { 200.0f,200.0f };//スタートマスの位置を参照
     rect={0, 0, m_dice_width, m_dice_height};
 }
 
 void Dice::Update(void)
 {
-    /*
-    * まだ位置を移動させるだけ
-    * 出目を変えて、同じ出目でもサイコロの向きによって次の出目が変わる処理を追加しないといけない
-    * 
-    * 例：向いてる方向によって1～4の番号付けをして、場合分けで次の出目を変える方法
-    * ↑次の出目の向き番号はどうやって判定する？
-    */
-    if (vivid::keyboard::Trigger(vivid::keyboard::KEY_ID::W))
+    int work = NULL;//入れ替えるのに必要
+
+    if (vivid::keyboard::Trigger(vivid::keyboard::KEY_ID::W) && m_Position.y >= 264.0f)
     {
         m_Position.y -= 64.0f;
-    } 
-    else if (vivid::keyboard::Trigger(vivid::keyboard::KEY_ID::A))
+        work = m_Dice.top;
+        m_Dice.top = m_Dice.center;
+        m_Dice.center = m_Dice.bottom;
+        m_Dice.bottom = m_Dice.back;
+        m_Dice.back = work;
+    }
+    else if (vivid::keyboard::Trigger(vivid::keyboard::KEY_ID::A) && m_Position.x >= 264.0f)
     {
         m_Position.x -= 64.0f;
+        work = m_Dice.center;
+        m_Dice.center = m_Dice.right;
+        m_Dice.right = m_Dice.back;
+        m_Dice.back = m_Dice.left;
+        m_Dice.left = work;
     }
-    else if (vivid::keyboard::Trigger(vivid::keyboard::KEY_ID::S))
+    else if (vivid::keyboard::Trigger(vivid::keyboard::KEY_ID::S) && m_Position.y < m_map_height + 136.0f)
     {
         m_Position.y += 64.0f;
+        work = m_Dice.top;
+        m_Dice.top = m_Dice.back;
+        m_Dice.back = m_Dice.bottom;
+        m_Dice.bottom = m_Dice.center;
+        m_Dice.center = work;
     }
-    else if (vivid::keyboard::Trigger(vivid::keyboard::KEY_ID::D))
+    else if (vivid::keyboard::Trigger(vivid::keyboard::KEY_ID::D) && m_Position.x < m_map_width + 136.0f)
     {
         m_Position.x += 64.0f;
+        work = m_Dice.center;
+        m_Dice.center = m_Dice.left;
+        m_Dice.left = m_Dice.back;
+        m_Dice.back = m_Dice.right;
+        m_Dice.right = work;
     }
+    rect.left = m_dice_width * (m_Dice.center - 1);
+    rect.right = m_dice_width * m_Dice.center;
 }
 
 void Dice::Draw(void)
 {
     vivid::DrawTexture("data\\gamemain_utility\\dice.png", m_Position, 0xffffffff, rect);
+    vivid::DrawText(40, std::to_string(m_Dice.center), { 0.0f,100.0f });
 }
 
 int Dice::GetDiceDigit(void)
 {
-    return 6;
+    return m_Dice.center;
 }
--- 00_vivid2d 2.8 pazle/game/src/game/game_object/GameMain/dice/dice.h
+++ 00_vivid2d 2.8 pazle/game/src/game/game_object/GameMain/dice/dice.h
@@ -6,10 +6,13 @@ class Dice
 private:
 	int m_DiceDigit; //ダイスの出目
 	int m_BlastCount;//爆破回数
+	int m_DiceDirection;//サイコロの向き
 	vivid::Vector2 m_Position;
 	vivid::Rect rect;
 	const int m_dice_width = 64;
 	const int m_dice_height = 64;
+	const float m_map_width = 768;
+	const float m_map_height = 768;
 
 public:
 	void Initialize(void);
