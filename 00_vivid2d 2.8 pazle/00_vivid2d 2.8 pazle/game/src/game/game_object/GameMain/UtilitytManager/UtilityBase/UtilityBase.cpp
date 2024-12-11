c6b3a3ee3c9f426dca7c4247ee7c54131c819b7e
yuukiutr <151480316+yuukiutr@users.noreply.github.com>
2024-12-11 10:27:08 +0900

    up

--- 00_vivid2d 2.8 pazle/00_vivid2d 2.8 pazle/game/src/game/game_object/GameMain/UtilitytManager/UtilityBase/UtilityBase.cpp
+++ 00_vivid2d 2.8 pazle/00_vivid2d 2.8 pazle/game/src/game/game_object/GameMain/UtilitytManager/UtilityBase/UtilityBase.cpp
@@ -8,17 +8,15 @@ const int UtilityBase::m_utility_height = 64;
 const bool UtilityBase::m_UtilityFlag = false;
 
 
-void UtilityBase::Initialize(Dice* dice, vivid::Vector2 pos)
+void UtilityBase::Initialize(vivid::Vector2 pos)
 {
-    m_Dice = dice;
-
     m_UtilityPosition = pos;
     m_UtilityColor = 0xffffffff;
 }
 
 void UtilityBase::Update(void)
 {
-    vivid::Vector2 vec2 = m_Dice->GetDicePosition();
+    vivid::Vector2 vec2 = Dice::GetInstance().GetDicePosition();
     
 }
 
@@ -32,6 +30,7 @@ void UtilityBase::Finalize(void)
 
 bool UtilityBase::GetCollisionFlag(void)
 {
+<<<<<<< HEAD
     bool hitflag = false;
 
     vivid::Vector2 vec2 = m_Dice->GetDicePosition();
@@ -44,6 +43,15 @@ bool UtilityBase::GetCollisionFlag(void)
         && vec2.x < m_UtilityPosition.x + (float)m_utility_width + velo.x - 1.0f
         && vec2.y + (float)height >= m_UtilityPosition.y
         && vec2.y <= m_UtilityPosition.y - (float)height / 2.0f)
+=======
+    vivid::Vector2 pos = Dice::GetInstance().GetDicePosition();
+    int width = Dice::GetInstance().GetDiceWidth();
+    int height = Dice::GetInstance().GetDiceHeight();
+    if (pos.x + (float)width > m_UtilityPosition.x
+        && pos.x < m_UtilityPosition.x + (float)m_utility_width
+        && pos.y + (float)height > m_UtilityPosition.y
+        && pos.y < m_UtilityPosition.y + (float)m_utility_height)
+>>>>>>> aa0a2e87fced97806b6c528a0bea7c7800448fc9
     {
         hitflag = true;
     }
